041d11e2e81a9ad161bb7f414fd3a435837517a5
jankin <jankinliu@163.com>
2019-01-24 01:04:49 +0800

    Test2 patch

---
+++ src/main/java/com/jankin/Test2.java
@@ -0,0 +1,11 @@
+package com.jankin;
+
+/**
+ * @author jankin
+ * @date 2019/1/24
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2....");
+    }
+}
